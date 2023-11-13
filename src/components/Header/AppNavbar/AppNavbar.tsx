8bd466cd33a8fafdd49c7204d9ccbf199c7e1f70
Gerard Heredia <gerard.heredia.bcn@gmail.com>
2023-11-13 17:40:14 +0100

    Header animations

--- src/components/Header/AppNavbar/AppNavbar.tsx
+++ src/components/Header/AppNavbar/AppNavbar.tsx
@@ -1,28 +1,19 @@
-import { Navbar, Typography } from "@material-tailwind/react";
-import React from "react";
+import { IconButton, Navbar, Typography } from "@material-tailwind/react";
 import { Link } from "react-router-dom";
+import HeaderLink from "../HeaderLink/HeaderLink";
 
 function NavList() {
   return (
     <div className="my-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-12">
-        <Link to="/" className="flex items-center font-black hover:text-tertiary transition-colors">
-          Home
-        </Link>
-        <Link to="/about" className="flex items-center font-black hover:text-tertiary transition-colors">
-          About
-        </Link>
-        <Link to="/projects" className="flex items-center font-black hover:text-tertiary transition-colors">
-          Projects
-        </Link>
-        <Link to="/contact" className="flex items-center font-black hover:text-tertiary transition-colors">
-          Contact
-        </Link>
-          </div>
+      <HeaderLink title="Home" href="/" />
+      <HeaderLink title="About" href="/about" />
+      <HeaderLink title="Projects" href="/projects" />
+      <HeaderLink title="Contact" href="/contact" />
+    </div>
   );
 }
 
 export default function AppNavbar() {
-
   return (
     <Navbar className="my-auto mx-auto max-w-screen-2xl px-3 py-6">
       <div className="flex items-center justify-between">
