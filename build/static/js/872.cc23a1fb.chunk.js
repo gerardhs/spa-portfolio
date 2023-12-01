85f0df13bbc7b449fc52e99c5b5e9b5e86763587
Gerard Heredia <gerard.heredia.bcn@gmail.com>
2023-12-02 00:47:04 +0100

    build

---
+++ build/static/js/872.cc23a1fb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkspa_portfolio=self.webpackChunkspa_portfolio||[]).push([[872],{872:function(o,e,n){n.r(e);var t=n(184);e.default=function(){return(0,t.jsx)("div",{children:"Home Component"})}}}]);
+//# sourceMappingURL=872.cc23a1fb.chunk.js.map
