7c5153f406248b4e55eaa173d2266a7c3787cb0d
Gerard Heredia <gerard.heredia.bcn@gmail.com>
2023-12-10 18:12:18 +0100

    build

---
+++ build/static/js/872.7473c1e3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkspa_portfolio=self.webpackChunkspa_portfolio||[]).push([[872],{872:function(e,s,a){a.r(s),a.d(s,{default:function(){return r}});var l=a(230),n=a(184);function r(){var e=(0,l.$G)().t;return(0,n.jsx)("div",{className:"flex-grow mx-auto max-w-screen-xl",children:(0,n.jsxs)("div",{children:[(0,n.jsx)("h2",{className:"font-medium text-6xl",children:e("pages.home.name")}),(0,n.jsx)("p",{className:"text-4xl",children:e("pages.home.role")})]})})}}}]);
+//# sourceMappingURL=872.7473c1e3.chunk.js.map
