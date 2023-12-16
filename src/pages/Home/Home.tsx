59eff79a0678fefa23521e76aab857f18c2dff73
Gerard Heredia <gerard.heredia.bcn@gmail.com>
2023-12-17 00:26:38 +0100

    Fix underline width bug

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,25 +1,14 @@
 import { useTranslation } from "react-i18next";
 import { HomeGraphic } from "../../components/shared/graphics";
-import { useEffect, useState } from "react";
 
 export default function Home() {
   const { t } = useTranslation();
 
-  useEffect(() => {
-    // Update the width of div2 to be the same as div1
-    let one: any = document.getElementById("reveal-div");
-    let two: any = document.getElementById("underline-div");
-    let style: any = window.getComputedStyle(one);
-    let wdt = style.getPropertyValue("width");
-    two.style.width = wdt;
-  }, []);
-
   return (
     <div className="flex-grow mx-auto max-w-screen-xl xl:p-0 p-2 ">
       <div className={`flex justify-between items-end`}>
-        <div id="reveal-div" className="reveal">
-          <h2 className="font-medium xl:text-6xl lg:text-4xl md:text-3xl sm:text-2xl text-md w-full">
-            <span></span>
+        <div className="reveal">
+          <h2 className="font-medium xl:text-6xl lg:text-4xl md:text-3xl sm:text-2xl text-md">
             {t("pages.home.name")}
           </h2>
           <p className="xl:text-4xl lg:text-2xl md:text-xl sm:text-lg text-sm">
@@ -30,10 +19,7 @@ export default function Home() {
           <HomeGraphic />
         </div>
       </div>
-      <div
-        id="underline-div"
-        className="reveal-underline md:h-4 h-[0.6rem]"
-      ></div>
+      <div className="reveal-underline w-0 md:h-4 h-[0.6rem]"></div>
     </div>
   );
 }
