93722ccf59dc9138198bd413c88af741e2d6e2cd
Gerard Heredia <gerard.heredia.bcn@gmail.com>
2023-12-17 01:07:34 +0100

    redone  fix

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from "react-i18next";
 import { HomeGraphic } from "../../components/shared/graphics";
+import { MutableRefObject, useLayoutEffect, useRef } from "react";
 
 export default function Home() {
   const { t } = useTranslation();
@@ -7,7 +8,7 @@ export default function Home() {
   return (
     <div className="flex-grow mx-auto max-w-screen-xl xl:p-0 p-2 ">
       <div className={`flex justify-between items-end`}>
-        <div className="reveal">
+        <div className="reveal w-2/5">
           <h2 className="font-medium xl:text-6xl lg:text-4xl md:text-3xl sm:text-2xl text-md">
             {t("pages.home.name")}
           </h2>
@@ -15,11 +16,11 @@ export default function Home() {
             {t("pages.home.role")}
           </p>
         </div>
-        <div>
+        <div className="w-3/5">
           <HomeGraphic />
         </div>
       </div>
-      <div className="reveal-underline w-0 md:h-4 h-[0.6rem]"></div>
+      <div className="reveal-underline md:h-4 h-[0.6rem] w-2/5"></div>
     </div>
   );
 }
